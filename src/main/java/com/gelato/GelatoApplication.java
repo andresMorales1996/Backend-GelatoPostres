a3963746cde4111e6b62d203b84e3441922f14b0
andresMorales1996 <brayan.morales@genstudents.org>
2024-07-07 19:01:40 -0500

    Primer Commit

---
+++ src/main/java/com/gelato/GelatoApplication.java
@@ -0,0 +1,13 @@
+package com.gelato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GelatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GelatoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/gelato/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.gelato;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GelatoApplication.class);
+	}
+
+}
