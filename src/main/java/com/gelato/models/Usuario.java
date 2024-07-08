18846048e205feca8a4151a7d3f8dc70f8315584
Vane789 <anlly.cardona@genstudents.org>
2024-07-08 09:42:16 -0500

    se agrega datos usuario

--- src/main/java/com/gelato/models/Usuario.java
+++ src/main/java/com/gelato/models/Usuario.java
@@ -1,6 +1,7 @@
 package com.gelato.models;
 
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @AllArgsConstructor
@@ -8,6 +9,22 @@ import lombok.*;
 @Data
 @Entity
 
-
+@Table(name = "usuarios")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @Column(name = "ID_usuario")
+    private long ID_usuario;
+    @Column(name = "nombre_usuario")
+    private String nombre_usuario;
+    @Column(name = "telefono_usuario")
+    private String telefono_usuario;
+    @Column(name = "email_usuario")
+    private String email_usuario;
+    @Column(name = "contrasena_usuario")
+    private String contrasena_usuario;
+    @Column(name = "genero_usuario")
+    private String genero_usuario;
+    @Column(name = "imagen_usuario")
+    private String imagen_usuario;
 }
