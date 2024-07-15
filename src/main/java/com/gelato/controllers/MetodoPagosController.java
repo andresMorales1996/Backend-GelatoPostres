b9718114100eeda4cf6425820fec592f9a0065c2
andresMorales1996 <brayan.morales@genstudents.org>
2024-07-14 19:23:02 -0500

    Correción a pequeño error en controlador metodoPagos

--- src/main/java/com/gelato/controllers/MetodoPagosController.java
+++ src/main/java/com/gelato/controllers/MetodoPagosController.java
@@ -8,33 +8,33 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/metodo-pago/v1")
+@RequestMapping("/metodoPagos/v1")
 public class MetodoPagosController {
 
   @Autowired
   private MetodoPagoService metodoPagoService;
 
-  @GetMapping("AllPayMethods")
+  @GetMapping("allMetodoPagos")
   public List<MetodoPagos> getAllMetodosPago() {
     return metodoPagoService.getAllMetodosPago();
   }
 
-  @GetMapping("/PayMethod{id}")
+  @GetMapping("/metodoPago{id}")
   public MetodoPagos getMetodoPagoById(@PathVariable Long id) {
     return metodoPagoService.getMetodoPagoById(id);
   }
 
-  @PostMapping("/createPayMethod")
+  @PostMapping("/createMetodoPago")
   public MetodoPagos addMetodoPago(@RequestBody MetodoPagos metodoPagos) {
     return metodoPagoService.addMetodoPago(metodoPagos);
   }
 
-  @PutMapping("/updatePayMethod/{id}")
+  @PutMapping("/updateMetodoPago/{id}")
   public MetodoPagos updateMetodoPago(@PathVariable Long id, @RequestBody MetodoPagos metodoPagosDetails) {
     return metodoPagoService.updateMetodoPago(id, metodoPagosDetails);
   }
 
-  @DeleteMapping("/deletePayMethod/{id}")
+  @DeleteMapping("/deleteMetodoPago/{id}")
   public void deleteMetodoPago(@PathVariable Long id) {
     metodoPagoService.deleteMetodoPago(id);
   }
